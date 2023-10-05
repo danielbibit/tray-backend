7139ac8c35a5e9e63b9ed8337da15ea405173abf
Daniel Moraes <danielbibit@gmail.com>
2023-10-05 16:09:46 +0000

    Add SellerService unit tests

--- tests/Unit/SellerServiceTest.php
+++ tests/Unit/SellerServiceTest.php
@@ -2,15 +2,59 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+// use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use App\Services\SellerService;
+use App\Repositories\SellerRepository;
+use App\Models\Seller;
+use Mockery;
+use Mockery\MockInterface;
 
 class SellerServiceTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_that_true_is_true(): void
+    public function test_seller_email_validation(): void
     {
-        $this->assertTrue(true);
+        $sellerRepositoryMock = $this->createMock(SellerRepository::class);
+
+        // $sellerRepositoryMock->expects($this->once())->method('create')->willReturn(Seller::factory()->make());
+        $sellerRepositoryMock->method('create')->willReturn(Seller::factory()->make());
+        $sellerService = new SellerService($sellerRepositoryMock);
+
+        // echo "mock created";
+        try {
+            $sellerService->create([
+                'name' => 'John Doe',
+                'email' => 'test'
+            ]);
+
+            $this->fail('Validation should fail');
+        }catch( \ErrorException $e) {
+            $this->assertEquals('The email field must be a valid email address.', $e->getMessage());
+        }
+
+
+    }
+
+    public function test_seller_name_validation(): void
+    {
+        $sellerRepositoryMock = $this->createMock(SellerRepository::class);
+
+        // $sellerRepositoryMock->expects($this->once())->method('create')->willReturn(Seller::factory()->make());
+        $sellerRepositoryMock->method('create')->willReturn(Seller::factory()->make());
+        $sellerService = new SellerService($sellerRepositoryMock);
+
+        // echo "mock created";
+        try {
+            $sellerService->create([
+                'name' => 'Jo',
+                'email' => 'test@test.com'
+            ]);
+
+            $this->fail('Validation should fail');
+        }catch( \ErrorException $e) {
+            $this->assertEquals('The name field must be at least 3 characters.', $e->getMessage());
+        }
+
+
     }
 }
